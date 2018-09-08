868bd8547585e08701bd2f875b4a0fd0a864b480
Nitin <Nshirke003@gmail.com>
2018-09-08 08:47:56 +0530

    first commit

---
+++ GitUser1/src/com/git/Demo.java
@@ -0,0 +1,18 @@
+package com.git;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		System.out.println(addition(a, b));
+
+	}
+
+	public static int  addition(int a, int b) {
+		int c = a + b;
+		return c;
+	}
+
+}
