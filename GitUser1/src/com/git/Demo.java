325a22e6047cf9978c4a41f4d8e7685f48a774ab
Nitin <Nshirke007@gmail.com>
2018-09-08 09:55:51 +0530

    substration method added

--- GitUser1/src/com/git/Demo.java
+++ GitUser1/src/com/git/Demo.java
@@ -7,11 +7,17 @@ public class Demo {
 		int a = 10;
 		int b = 20;
 		System.out.println(addition(a, b));
-
+		System.out.println(substratioc(a, b));
 	}
 
-	public static int  addition(int a, int b) {
+	public static int addition(int a, int b) {
 		int c = a + b;
+		return c;
+	}
+
+	public static int substratioc(int a, int b) {
+
+		int c = a - b;
 		return c;
 	}
 
