1f6f542c8552332abaed0d5371e1e02202bed7d2
Nitin <Nshirke007@gmail.com>
2018-10-06 08:10:38 +0530

    sysout 3 added

--- GitUser1/src/com/git/Demo.java
+++ GitUser1/src/com/git/Demo.java
@@ -11,6 +11,8 @@ public class Demo {
 		System.out.println("helo......");
 		System.out.println("helo11111111......");
 		System.out.println("helo222222222......");
+
+		System.out.println("hello3333333.........");
 	}
 
 	public static int addition(int a, int b) {
@@ -21,12 +23,12 @@ public class Demo {
 	public static int substratioc(int a, int b) {
 
 		int c = a - b;
-	return c;
+		return c;
 	}
-	
-	public static int multiplication(int a,int b){
-		int c=a*b;
-		
+
+	public static int multiplication(int a, int b) {
+		int c = a * b;
+
 		return c;
 	}
 
