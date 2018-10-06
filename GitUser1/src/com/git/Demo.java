a39e260dfef44ec8f8c3176f8297dcab2dbc9a7e
Nitin <Nshirke007@gmail.com>
2018-10-06 07:55:59 +0530

    conflict error

--- GitUser1/src/com/git/Demo.java
+++ GitUser1/src/com/git/Demo.java
@@ -18,16 +18,10 @@ public class Demo {
 		return c;
 	}
 
-/*<<<<<<< HEAD*/
 	public static int substratioc(int a, int b) {
 
 		int c = a - b;
-/*=======
-	public static int multiplication(int a, int b) {
-		int c = a * b;
-
->>>>>>> branch 'master' of https://github.com/newmaxx/nitin.git
-*/		return c;
+		return c;
 	}
 
 }
